b6e0c48a80103a018826cf0f9801977b38e77c4e
d-randoll <51640895+d-randoll@users.noreply.github.com>
2022-10-30 20:55:47 -0400

    try out config from application.properties to pull welcome message

--- src/main/java/com/drandoll/Springboot/tutorial/Controller/HelloController.java
+++ src/main/java/com/drandoll/Springboot/tutorial/Controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.drandoll.Springboot.tutorial.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String _welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome to David APII";
+        return _welcomeMessage;
     }
 }
